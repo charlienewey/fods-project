02f13bd82fdb489d1ef22ca0b381fbd7f36a19b6
Charles Newey <charlie@assemblyco.de>
2016-12-16 00:36:58 +0000

    Add regression lines in scatter plot

--- server/static/js/charts.js
+++ server/static/js/charts.js
@@ -1,8 +1,3 @@
-Date.prototype.getFullYear = function () {
-  return this.getYear() + 1900;
-}
-
-
 /*
  * log-price/review scatter plot
  * adapted from: nvd3.org/examples/scatter.html
@@ -13,33 +8,47 @@ function drawLogPriceReviews(reviews) {
       .showDistX(true)  // marginal distribution on x axis
       .color(d3.scale.category10().range())
       .pointRange([25, 25])
-      .margin({top: 0, right: 0, bottom: 50, left: 100});
+      .margin({top: 0, right: 50, bottom: 50, left: 100});
 
     // format axes
-    chart.xScale(d3.scale.log().base(Math.E));
-    chart.xAxis.tickFormat(d3.format('.02f'))
-      .axisLabel('Price (log-scale)');
+    // chart.xScale(d3.scale.log().base(Math.E));
+    chart.xAxis.tickFormat(d3.format('.0f'))
+      .axisLabel('Price (log-scale)')
+      .tickFormat(function(d) { return Math.round(Math.exp(d)) });
 
-    chart.yAxis.tickFormat(d3.format('.02f'))
+    chart.yAxis.tickFormat(d3.format('.0f'))
       .axisLabel('Average Review Score');
 
-    // Configure how the tooltip looks.
+    // configure how the tooltip looks
     chart.tooltip.contentGenerator(function(key) {
       return '<b>Producer:</b> ' + key.point.producer + '<br />'+
              '<b>Vintage:</b> ' + key.point.vintage + '<br />' +
-             '<b>Average Price:</b> £' + key.point.x + '<br />' +
+             '<b>Average Price:</b> £' + Math.round(Math.exp(key.point.x)) + '<br />' +
              '<b>Average Review Score:</b> ' + key.point.y + '<br />';
     });
 
     // get data
     var data = [];
     var groups = ['Bordeaux', 'Burgundy', 'Northern Rhone', 'Southern Rhone'];
+    var regressions = [  // [y-intercept, slope]
+      [81.9289, 2.0717],  // Bordeaux
+      [82.8734, 1.9528],  // Burgundy
+      [82.2085, 2.4090],  // Northern Rhone
+      [76.6135, 3.9853]   // Southern Rhone
+    ]
     for (var i = 0; i < groups.length; i += 1) {
       // put data into array...
       data.push({
         key: groups[i],
-        values: []
+        values: [],
+        intercept: regressions[i][0],
+        slope: regressions[i][1]
       });
+
+      // disable half of the series, otherwise it gets confusing
+      if (i % 2 > 0) {
+        data[i].disabled = true;
+      }
 
       // extract fields
       for (var j = 0; j < reviews.length; j += 1) {
@@ -48,7 +57,8 @@ function drawLogPriceReviews(reviews) {
           var y = parseFloat(reviews[j].rating);
           if (! isNaN(x) && ! isNaN(y)) {
             data[i].values.push({
-              x: x, y: y,
+              x: Math.log(x),
+              y: y,
               producer: reviews[j].name,
               vintage: reviews[j].year
             })
@@ -79,16 +89,17 @@ function drawPriceByRegion(prices) {
       .showLegend(true)
       .showYAxis(true)
       .showXAxis(true)
-      .margin({top: 0, right: 0, bottom: 50, left: 100});
+      .color(d3.scale.category10().range())
+      .margin({top: 0, right: 50, bottom: 50, left: 100});
 
     chart.xAxis
-      .axisLabel('Time (ms)')
+      .axisLabel('Date')
       .tickFormat(function(d) {
         return d3.time.format('%Y-%m')(new Date(d))
       });
 
     chart.yAxis
-      .axisLabel('Voltage (v)')
+      .axisLabel('Price per Bottle (£)')
       .axisLabelDistance(3)
       .tickFormat(function(d) { return '£' + d3.format('.02f')(d) });
 
