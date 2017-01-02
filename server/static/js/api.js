d157cdc8f1920b2f4488c9f1931a1928ba5d84a7
Charles Newey <charlie@assemblyco.de>
2017-01-02 19:40:22 +0000

    Tidy up JS

--- server/static/js/api.js
+++ server/static/js/api.js
@@ -1,49 +1,27 @@
 // functions
-function getReviews(wine, rvw) {
-  if (wine === null) {
-    $.get("reviews", function (data) {
-      for (var i = 0; i < data.data.length; i += 1) {
-        rvw.push(data.data[i]);
-      }
-      drawLogPriceReviews(reviews);
-    });
-  } else {
-    $.get("reviews/" + wine, function (data) {
-      // probably ought to do something cleverer here
-      // do things with data.data
-    });
-  }
+function getReviews(rvw) {
+  $.get("/data/reviews", function (data) {
+    for (var i = 0; i < data.data.length; i += 1) {
+      rvw.push(data.data[i]);
+    }
+    drawLogPriceReviews(reviews);
+  });
 }
 
 function getWeather(wth) {
-  $.get("weather", function (data) {
+  $.get("/data/weather", function (data) {
     for (var i = 0; i < data.data.length; i += 1) {
       wth.push(data.data[i]);
     }
   });
 }
 
-function getPrices(wine, prc) {
-  if (wine === null) {
-    $.get("prices", function (data) {
-      for (var i = 0; i < data.data.length; i += 1) {
-        prc.push(data.data[i]);
-      }
-      drawPriceByRegion(prc);
-    });
-  } else {
-    $.get("prices/" + wine, function (data) {
-      // probably ought to do something cleverer here
-      // do things with data.data
-    });
-  }
-}
-
-function getMarket(mkt) {
-  $.get("market", function (data) {
+function getPrices(prc) {
+  $.get("/data/prices", function (data) {
     for (var i = 0; i < data.data.length; i += 1) {
-      mkt.push(data.data[i]);
+      prc.push(data.data[i]);
     }
+    drawPriceByRegion(prc);
   });
 }
 
@@ -51,12 +29,10 @@ function getMarket(mkt) {
 var reviews = [];
 var weather = [];
 var prices = [];
-var market = [];
 
 // set global variables with a horrible nasty nasty callback function
 $(document).ready(function () {
-  getMarket(market);
   getWeather(weather);
-  getReviews(null, reviews);
-  getPrices(null, prices);
+  getReviews(reviews);
+  getPrices(prices);
 });
